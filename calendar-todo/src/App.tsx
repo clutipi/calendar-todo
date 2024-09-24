eb120e147df186d31eaf856302256be040f6c5ef
DanielNG <daniel.gidrao@catijr.com.br>
2024-09-24 01:21:58 -0300

    feat: datepicker implemented

--- calendar-todo/src/App.tsx
+++ calendar-todo/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import styles from './App.module.css'
+import { DatePicker } from 'rsuite';
+import 'rsuite/dist/rsuite.min.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+function MyComponent() {
+     const [date, setDate] = useState<Date | null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     const handleDateChange = (newDate: Date | null) => {
+          setDate(newDate);
+          console.log(newDate);
+     };
+
+     return (
+          <div>
+               <h3>Escolha a data:</h3>
+               <DatePicker 
+                    className={styles.robotoMono}
+                    value={date} 
+                    onChange={handleDateChange} 
+                    placeholder="Selecione uma data" 
+                    format="dd/MM/yyyy"
+               />
+          </div>
+     );
 }
 
-export default App
+export default MyComponent;
